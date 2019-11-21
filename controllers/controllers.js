b59718ec7f1cba5e15434cf9612836b371a6ddf4
Kennedy-lab <52857230+Kennedy-lab@users.noreply.github.com>
2019-11-21 20:18:19 +0300

    Update controllers.js

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -12,19 +12,19 @@ const fs = require('fs');
 
 
 const pool = new Pool({
-	user:process.env.USER,
-	host:process.env.HOST,
-	database:process.env.DATABASE,
-	password:process.env.PASSWORD,
-	port:process.env.PORT,
+	user:'postgres',
+	host:'localhost',
+	database:'Teamwork employees',
+	password:'PostgreSQL',
+	port:5432,
 });
 
 const client = new Client({
-	user:process.env.USER,
-	host:process.env.HOST,
-	database:process.env.DATABASE,
-	password:process.env.PASSWORD,
-	port:process.env.PORT,
+	user:'postgres',
+	host:'localhost',
+	database:'Teamwork employees',
+	password:'PostgreSQL',
+	port:5432,
 });
 client.connect()
    .then(() => {
